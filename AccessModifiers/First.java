29c53178044b95e37375f6f9551aeda403fe6a97
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-03-25 23:07:59 +0300

    first commit

---
+++ AccessModifiers/First.java
@@ -0,0 +1,50 @@
+package AccessModifiers;
+
+public class First {
+
+    private String str;
+
+    First(String str){
+        this.str = str;
+    }
+
+    public String getStr (){
+        return this.str;
+    }
+
+}
+
+/*
+ * Private Erişim Belirleyici
+ * 
+ * 
+ * Java'da private deyimi yazıldığı öğenin sadece ait olduğu sınıftan doğrudan
+ * erişilebilir olduğunu ve o sınıfın dışındaki kod parçacıklarından doğrudan
+ * erişilemeyeceğini tanımlar. Nesne Yönelimli Programlama'nın temel
+ * ilkelerinden olan Sarmalama ilkesi gereği, sınıf içindeki değişkenler sadece
+ * sınıf içinde doğrudan erişebilir olması gerekir. Bundan dolayı, genellikle
+ * değişkenler "private" olarak tanımlanır. Bazı zamanlarda ise sadece o sınıfta
+ * çağrılmasını istediğimiz değişkenler veya metotları da private olarak
+ * tanımlarız.
+ * 
+ * 
+ * 
+ * Public Erişim Belirleyici
+ * 
+ * 
+ * Java'da "public" deyimi , yazıldığı öğenin sadece olduğu sınıf için değil,
+ * diğer sınıflar tarafından doğrudan erişilebilir olmasını sağlar. Sınıflara
+ * ait nesnelerin ve diğer nesneler tarafından kullanılması istenilen metotlar
+ * için "Public Erişim Düzenleyicisi" kullanılır.
+ * 
+ * 
+ * 
+ * Protected Erişim Belirleyici
+ * 
+ * 
+ * Java'da "protected" deyimi , public ve private arasında kalan bir erişim
+ * düzenleyicidir. Protected ile tanımlanan öğeler, kendisi ile aynı package
+ * (paket) bulunan sınıflar tarafından doğrudan erişilir.
+ * 
+ * 
+ */
---
+++ AccessModifiers/Main.java
@@ -0,0 +1,13 @@
+package AccessModifiers;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        First a = new First("hasan");
+        System.out.println(a.getStr());
+
+
+
+    }
+}
