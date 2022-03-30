0c3d7d56474d3c428403e0f25d037c429b64965f
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-03-30 18:31:29 +0300

    GenericClass

---
+++ NestedClassesAndGenerics/Generics/Main.java
@@ -0,0 +1,32 @@
+package NestedClassesAndGenerics.Generics;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Undifined
+        //String s;
+
+        // Null
+        //String s1 = null;
+
+        // Empty
+        //String s2 = "";
+
+        // We need to write a class that checks if all data types are null
+        Nullable<Integer> n = new Nullable<Integer>(10);
+        System.out.println(n.getValue());
+        n.run();
+
+        Integer a = 10;
+        String b = "patika";
+        Double d = 3.14;
+
+        Test<Integer,String,Double> t = new Test<>(a,b,d);
+
+        t.showInfo();
+
+
+    }
+
+}
