1e0b36edd0d61009704a0b5eca7302c4d22b9bd5
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-04-02 23:13:56 +0300

    LinkedMapAndTreeMap

---
+++ Collection/LinkedMapAndTreeMap.java
@@ -0,0 +1,35 @@
+package Collection;
+
+
+import java.util.*;
+
+import Collection.Student;
+
+
+public class LinkedMapAndTreeMap {
+
+    public static void main(String[] args) {
+
+        // LinkedHashMap<Integer,Student> students = new LinkedHashMap<>();
+
+        TreeMap<Integer,Student> students = new TreeMap<>(new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+
+                return o1-o2;
+            }
+            
+        });
+
+        students.put(101, new Student("Hasan",100));
+        students.put(102, new Student("Selim", 1000));
+
+        for (Student stu : students.values()) {
+            System.out.println(stu.getName());
+        }
+
+
+    }
+    
+}
