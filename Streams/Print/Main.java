d665ae53934cd64ef6c0ea9fbf8a81cb6377a201
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-04-04 12:20:27 +0300

    Print Stream

---
+++ Streams/Print/Main.java
@@ -0,0 +1,23 @@
+package Print;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Çıktı verilerini byte yerine direk olarak metine çeviren koca yürekli bir yapı.
+
+        try {
+            PrintStream outPut = new PrintStream("Streams/Print/print.txt");
+            outPut.print("Selamlar");
+            outPut.print(1231231); // integer değer girsek de farketmiyor.
+            outPut.print(" Umarım çok iyisinizdir.");
+            outPut.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
