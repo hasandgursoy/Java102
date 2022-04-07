53493510554bb36cd1c54b1f65fb86095257d887
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-04-07 13:55:57 +0300

    Stream API

--- StreamAPIs/Main.java
+++ StreamAPIs/Main.java
@@ -1,6 +1,7 @@
 package StreamAPIs;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,10 +29,43 @@ public class Main {
         //Distance (tekrar eden sayıları yazmaz.)
         list.stream().distinct().forEach(i->System.out.println(i));
 
+        System.out.println("Sorted");
+        //Sorted (sıralama yapar) (reverseOrder() unutma lazım olcak)
+        list.stream().sorted(Comparator.reverseOrder()).forEach(i -> System.out.println(i));
+        
+        System.out.println("Limit");
+        //Limit (Belirli sayıda eleman alma fonksiyonu)
+        list.stream().limit(5).forEach(i->System.out.println(i));
 
+        System.out.println("Skip");
+        //Skip (Belir sayıda elemanı atlar)
+        list.stream().skip(3).limit(5).distinct().sorted(Comparator.reverseOrder()).forEach(a -> System.out.println(a));
 
+        System.out.println("Count");
+        //Count (Eleman sayısını söyler)
+        long c = list.stream().skip(1).limit(5).count();
+        System.out.println(c);
+
+        System.out.println("AnyMatch");
+        //AnyMatch (Eşleşen koşul varsa ture yoksa false döndürür)
+        boolean control = list.stream().anyMatch(i -> i < 10);
+        System.out.println(control);
+
+        System.out.println("AllMatch");
+        //allMatch (Bütün koşular eşleşiyorsa ture yoksa false döndürür)
+        boolean control2 = list.stream().allMatch(i -> i  < 100);
+        System.out.println(control2);
+    
+        System.out.println("NoneMatch");
+        //noneMatch (Eşleşmeyen koşul varsa ture yoksa false döndürür)
+        boolean control3 = list.stream().noneMatch(i -> i < 10);
+        System.out.println(control3);
+        
+        System.out.println("Map");
+        // map (liste içindeki elemanlar üzerinde işlemler yapmamızı sağlıyor)
+        list.stream().map(i->i*2).forEach(i -> System.out.println(i));
         
 
-
     }
-}
+
+}   
